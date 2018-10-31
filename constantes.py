7b07c4db9935673f82358d2169e7415b27cbc012
josecalcano <jose.calcano07@gmail.com>
2018-10-30 22:33:57 -0400

    Agregando estructura base

---
+++ constantes.py
@@ -0,0 +1,31 @@
+PRECIOS_TAMANO={
+    "g": 580,
+    "m": 430,
+    "p": 280
+}
+
+TAMANO = {
+    "g": "Grande",
+    "m": "Mediana",
+    "p": "Personal"
+}
+
+PRECIOS_TOPPING={
+    "ja": 40,
+    "ch": 35,
+    "pi": 30,
+    "dq": 40,
+    "ac": 57.5,
+    "pp": 38.5,
+    "sa": 62.5
+}
+
+TOOPING = {
+    "ja": "Jamon",
+    "ch": "Champinones",
+    "pi": "Pimenton",
+    "dq": "Doble Queso",
+    "ac": "Aceitunas",
+    "pp": "Pepperoni",
+    "sa": "Salchichon"
+}
---
+++ pizzeria.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+import os
+import constantes
+
+print("Portu Pizza")
+
+print("Bienvenido a Portu Pizza")
+
+cantPizzas = int(input('Ingrese la cantidad de pizzas que desea ordenar: '))
+
+print(cantPizzas)
+
+i=1
+
+while(i<=cantPizzas):
+    print("Pizza numero "+ str(i))
+    tamanoPizza=""
+    precioPizza=0
+    while((tamanoPizza!="g")and(tamanoPizza!="p")and(tamanoPizza!="m")):
+        print("Tamanos: Grande(g) Mediana(m) Personal (p)")
+        tamanoPizza = input('Ingrese el tamano de la pizza '+str(i)+":")
+        print (constantes.TAMANO.get(tamanoPizza, "Debes seleccionar el tamano correcto"))
+        print ("Selecciono el tamano "+str(constantes.TAMANO.get(tamanoPizza)))
+    
+    print("Opciones: Si(s) No(n)")
+    respuesta=input("Desea ingredientes adicionales?")
+    
+    while(respuesta=="s"):
+        print("Con ingredientes adicionales")
+    if(respuesta=="n"):
+        print("Usted ordeno una pizza de tamano "+ constantes.TAMANO.get(tamanoPizza) +" sin ingredientes adicionales")
+        precioPizza=constantes.PRECIOS_TAMANO.get(tamanoPizza)
+        print("Por un total de "+str(precioPizza) )
+    i=i+1
